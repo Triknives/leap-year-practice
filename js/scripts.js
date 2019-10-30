1009ef765d1de83f4624114bfe3468c705738f94
BrettCordell and LiamPKenna <liampkenna@>
2019-10-30 10:30:20 -0700

    add UI and refactor leapYear function

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
+
 var leapYear = function(year) {
-  return (year % 4 === 0 && (year % 100 != 0 || year % 400 === 0));
+  var yearInt = parseInt(year);
+  return (yearInt % 4 === 0 && (year % 100 != 0 || year % 400 === 0));
 };
 
 
-
-var tests = [
-  [1993, false],
-  [2004, true],
-  [1900, false],
-  [2000,true]
-]
-
-tests.forEach(function(thisTest) {
-  console.log(leapYear(thisTest[0]) === thisTest[1])
+// USER INTERFACE
+$(document).ready(function() {
+  $("#form1").submit(function(event) {
+    event.preventDefault();
+    $("#result").text(leapYear($("#yearInput").val()));
+  });
 });
 
 
 
-// $(document).ready(function() {
+// var tests = [
+//   [1993, false],
+//   [2004, true],
+//   [1900, false],
+//   [2000,true]
+// ]
 //
+// tests.forEach(function(thisTest) {
+//   console.log(leapYear(thisTest[0]) === thisTest[1])
 // });
+// function() {}
